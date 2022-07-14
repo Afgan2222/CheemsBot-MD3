beb12a0c0fab61de19e26a0a73ef5ad3fda8b7c7
Afgan2222 <108980042+Afgan2222@users.noreply.github.com>
2022-07-14 17:56:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,26 +24,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc"
-global.ytname = "YT:ARULL OFC " //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
+global.owner = ['6282125968810'] //ur owner number
+global.ownername = "Afgan  ID"
+global.ytname = "YT : --" //ur yt chanel name
+global.socialm = "GitHub: Afgan2222" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
-global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
+global.botname = "kanna BOT" //ur bot name
+global.websitex = "https://youtube.com" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
-global.author = "ArullBotz"
+global.author = "kanna BOT"
 global.themeemoji = "📀"
 global.reactmoji = "🗿"
-global.ownertag = ['ArullOfc'] //ur owner tag
-global.ownernummenu = ['6285822347348'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
-global.botscript = "https://youtu.be/Gr6yuCbSGTI"
-global.linkz1 = "https://youtu.be/Gr6yuCbSGTI"
-global.linkz2 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKASl"
+global.ownertag = ['Afgan  ID'] //ur owner tag
+global.ownernummenu = ['6282125968810'] //ur owner number in menu and all
+global.watermark = "Afgan  Official" //ur watermark
+global.botscript = "https://youtu.be"
+global.linkz1 = "https://youtu.be"
+global.linkz2 = "https://chat.whatsapp.com/DVfawsZozt36QdYmfQlsbD"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
